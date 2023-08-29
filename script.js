fc4bb768b0189a55f01772195f2b98c748386db6
meganm672 <meganm672@gmail.com>
2023-08-29 09:14:34 -0400

    fixed form to appear

--- script.js
+++ script.js
@@ -46,103 +46,103 @@ const addNewSong = async (song) => {
     }
 };
 
-const renderNewSongForm = () => {
-    //place form into div
-    const formContainer= document.querySelector('#new-song-form');
-    //create form element
-    const songForm = document.createElement('form');
-    //title lable and input
-    const titleLabel = document.createElement('label');
-    titleLabel.htmlFor = 'title';
-    titleLabel.textContent ='Title';
+// const renderNewSongForm = () => {
+//     //place form into div
+//     const formContainer= document.querySelector('#new-song-form');
+//     //create form element
+//     const songForm = document.createElement('form');
+//     //title lable and input
+//     const titleLabel = document.createElement('label');
+//     titleLabel.htmlFor = 'title';
+//     titleLabel.textContent ='Title';
 
-    const titleInput = document.createElement('input');
-    titleInput.type = 'text';
-    titleInput.id = 'title';
-    titleInput.name = 'title';
-    titleInput.placeholder = "Enter title here"
-    //artist label and input
-    const artistLabel = document.createElement('label');
-    artistLabel.htmlFor = 'artist';
-    artistLabel.textContent ='Artist';
+//     const titleInput = document.createElement('input');
+//     titleInput.type = 'text';
+//     titleInput.id = 'title';
+//     titleInput.name = 'title';
+//     titleInput.placeholder = "Enter title here"
+//     //artist label and input
+//     const artistLabel = document.createElement('label');
+//     artistLabel.htmlFor = 'artist';
+//     artistLabel.textContent ='Artist';
 
-    const artistInput = document.createElement('input');
-    artistInput.type = 'text';
-    artistInput.id = 'artist';
-    artistInput.name = 'artist';
-    artistInput.placeholder = "Enter artist here"
-    //genre lable and input
-    const genreLabel = document.createElement('label');
-    genreLabel.htmlFor = 'genre';
-    genreLabel.textContent ='genre';
+//     const artistInput = document.createElement('input');
+//     artistInput.type = 'text';
+//     artistInput.id = 'artist';
+//     artistInput.name = 'artist';
+//     artistInput.placeholder = "Enter artist here"
+//     //genre lable and input
+//     const genreLabel = document.createElement('label');
+//     genreLabel.htmlFor = 'genre';
+//     genreLabel.textContent ='genre';
 
-    const genreInput = document.createElement('input');
-    genreInput.type = 'text';
-    genreInput.id = 'genre';
-    genreInput.name = 'genre';
-    genreInput.placeholder = "Enter genre here"
-    //release date label and input
-    const releaseDateLabel = document.createElement('label');
-    releaseDateLabel.htmlFor = 'releaseDate';
-    releaseDateLabel.textContent ='releaseDate';
+//     const genreInput = document.createElement('input');
+//     genreInput.type = 'text';
+//     genreInput.id = 'genre';
+//     genreInput.name = 'genre';
+//     genreInput.placeholder = "Enter genre here"
+//     //release date label and input
+//     const releaseDateLabel = document.createElement('label');
+//     releaseDateLabel.htmlFor = 'releaseDate';
+//     releaseDateLabel.textContent ='releaseDate';
 
-    const releaseDateInput = document.createElement('input');
-    releaseDateInput.type = 'date';
-    releaseDateInput.id = 'releaseDate';
-    releaseDateInput.name = 'releaseDate';
-    releaseDateInput.placeholder = "Enter release date here"
+//     const releaseDateInput = document.createElement('input');
+//     releaseDateInput.type = 'date';
+//     releaseDateInput.id = 'releaseDate';
+//     releaseDateInput.name = 'releaseDate';
+//     releaseDateInput.placeholder = "Enter release date here"
 
-    //submit button
-    const submitButton = document.createElement('button');
-    submitButton.type = 'submit';
-    submitButton.textContent = 'Submit'
+//     //submit button
+//     const submitButton = document.createElement('button');
+//     submitButton.type = 'submit';
+//     submitButton.textContent = 'Submit'
 
-    //append the form
-    songForm.append(
-        formContainer,
-        titleLabel,
-        titleInput,
-        artistLabel,
-        artistInput,
-        genreLabel,
-        genreInput,
-        releaseDateLabel,
-        releaseDateInput,
-        submitButton,
-    );
+//     //append the form
+//     songForm.append(
+//         formContainer,
+//         titleLabel,
+//         titleInput,
+//         artistLabel,
+//         artistInput,
+//         genreLabel,
+//         genreInput,
+//         releaseDateLabel,
+//         releaseDateInput,
+//         submitButton,
+//     );
 
-    songForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
+//     songForm.addEventListener('submit', async (event) => {
+//         event.preventDefault();
 
-        const form = event.target;
+//         const form = event.target;
 
-        const songData = {
-            title : form.title.value,
-            artits: form.artist.value,
-            genre: form.genre.value,
-            releaseDate : form.releaseDate.value,
-        }
-        await addNewSong(songData);
-        const songs = await fetchAllSongs();
-        renderAllSongs(songs);
-    })
-        renderNewSongForm.appendChild(songForm);
-//     const newSongForm = document.querySelector('#new-song-form');
-//     newSongForm.innerHTML = `
-//       <form>
-//         <label for="title">Title</label>
-//         <input type="text" name="title" id="title" />
-//         <label for="artist">Artist</label>
-//         <input type="text" name="artist" id="artist" />
-//         <label for="genre">Genre</label>
-//         <input type="text" name="genre" id="genre" />
-//         <label for="release-date">Release Date</label>
-//         <input type="date" name="release-date" id="release-date" />
-//         <button type="submit">Submit</button>
-//       </form>
-//     `;
-//   }
-}
+//         const songData = {
+//             title : form.title.value,
+//             artits: form.artist.value,
+//             genre: form.genre.value,
+//             releaseDate : form.releaseDate.value,
+//         }
+//         await addNewSong(songData);
+//         const songs = await fetchAllSongs();
+//         renderAllSongs(songs);
+//     })
+//         renderNewSongForm.appendChild(songForm);
+    const newSongForm = document.querySelector('#new-song-form');
+    newSongForm.innerHTML = `
+      <form>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" />
+        <label for="artist">Artist</label>
+        <input type="text" name="artist" id="artist" />
+        <label for="genre">Genre</label>
+        <input type="text" name="genre" id="genre" />
+        <label for="release-date">Release Date</label>
+        <input type="date" name="release-date" id="release-date" />
+        <button type="submit">Submit</button>
+      </form>
+    `;
+  
+
 //create async init funciton 
 // Call the `fetchAllSongs` function inside your `init` function
 //  and console.log the result. 
